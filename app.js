2d2dd6a0bb2e462d5739c4c09255dc9ff66024c6
WEBundleXYZ <stefg@webundlexyzsmbp.mynetworksettings.com>
2022-01-20 18:52:43 -0500

    Http requests on port 3000

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = require('http');
 
-const server = http.createServer();
+function handleRequest(request, response) {
+	response.statusCode = 200; // this is a propriety and not a method
+	response.end('<h1>Hi there</h1>');
+}
+
+const server = http.createServer(handleRequest);
 
 server.listen(3000);
-
